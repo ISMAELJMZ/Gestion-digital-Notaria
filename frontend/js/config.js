eb903f9c4dc23657cbac877dedcb64add43573c0
ISMAELJMZ <al18020082@itsa.edu.mx>
2025-06-19 23:54:19 -0600

    nueva url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,6 +3,6 @@ const currentHost = window.location.hostname;
 
 const API_BASE_URL = localHosts.includes(currentHost)
   ? "http://localhost:3000/api"
-  : "https://g47pzrrn-3000.usw3.devtunnels.ms/api";
+  : " https://notaria-publica-172.onrender.com/api";
 
 export default API_BASE_URL;
